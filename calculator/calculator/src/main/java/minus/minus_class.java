a4ad51e4b791e109292b8ee66375032831675858
BOOK-BVBUIR6SCP\amk02 <amk0223@naver.com>
2025-08-29 15:46:51 +0900

    20250829 commit

--- calculator/calculator/src/main/java/minus/minus_class.java
+++
@@ -1,7 +0,0 @@
-package minus;
-
-public class minus_class {
-    public int calculate(int a, int b) {
-        return a - b;
-    }
-}
--- calculator/calculator/src/main/java/mutiple/MutipleNumbers.java
+++
@@ -1,7 +0,0 @@
-package mutiple;
-
-public class MutipleNumbers {
-    public int calculate(int a, int b) {
-        return a * b;
-    }
-}
